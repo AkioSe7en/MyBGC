b983c95a0752878263e75ff3bc4a0a2501836976
Akio <akiose7en@bk.ru>
2023-05-31 16:34:36 +0700

    Добавьте файлы проекта.

---
+++ MyBGC/MyBGC/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyBGC
+{
+    public partial class MainPage : Shell
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            for (int i = 0; i < 150; i++)
+            {
+             // stack.Children.Add(new Label{Text = "Строка номер "+i});  
+            }
+        }
+    }
+}
---
+++ MyBGC/MyBGC/RingShedule.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyBGC
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RingShedule : ContentPage
+	{
+		public RingShedule ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
